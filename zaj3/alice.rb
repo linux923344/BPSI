df84c063b30af49a0141391a30db1f8ce8ed27c1
Marcin Wozniak <marcinwozniak@zoho.com>
2019-11-05 22:29:37 +0100

    Updated

---
+++ zaj3/alice.rb
@@ -0,0 +1,63 @@
+#!/usr/bin/env ruby
+
+require 'prime'
+require 'openssl'
+require 'socket'
+require 'securerandom'
+
+
+message = File.read("message.txt")
+
+dh1=OpenSSL::PKey::DH.new(512)
+dh1.generate_key!
+der = dh1.public_key.to_der
+
+sock = TCPSocket.new("localhost",5632)
+puts sock.gets  
+mode = gets.gsub(/\n$/, '')
+sock.puts mode
+
+sock.puts der
+sock.puts dh1.pub_key
+
+key=sock.gets
+key=key.chop
+key=key.to_i
+
+symm_key1 = dh1.compute_key(key.to_bn)
+
+c = OpenSSL::Cipher.new(mode)
+c.encrypt
+
+case mode.strip 
+when "des-ecb"
+    c.key = symm_key1[0..7]
+when "des-ede3-cbc"
+    c.key = symm_key1[0..23]
+    iv = c.iv = SecureRandom.random_bytes(8)
+    sock.puts iv
+when "aes-cbc-192"
+    c.key = symm_key1[0..23]
+    iv = c.iv = SecureRandom.random_bytes(16)
+    sock.puts iv
+when "idea-ofb"
+    iv = c.iv = SecureRandom.random_bytes(8)
+    sock.puts iv
+end
+
+encrypted = c.update(message) + c.final
+sock.puts encrypted
+
+puts
+puts "-----------------------------------"
+puts
+puts "Mode: " + mode
+puts "Public: " + (dh1.pub_key).to_s
+puts "Priv: " + (key).to_s
+puts "Sym Key 1: " + symm_key1
+puts "IV: " + iv.inspect
+puts "Encypted: " + encrypted
+puts
+puts "-----------------------------------"
+
+sock.close 
---
+++ zaj3/bob.rb
@@ -0,0 +1,61 @@
+#!/usr/bin/env ruby
+
+require 'prime'
+require 'openssl'
+require 'socket'
+require 'securerandom'
+
+sock = TCPServer.new 5632
+client = sock.accept
+
+client.puts "Choose: des-ecb, des-ede3-cbc, aes-cbc-192, rc5-ecb, idea-ofb"
+mode = client.gets.gsub(/\n$/, '')
+
+pp=client.gets
+pp=pp.chop
+
+dh2 = OpenSSL::PKey::DH.new(pp)
+dh2.generate_key!
+
+client.puts dh2.pub_key
+
+key=client.gets
+key=key.chop
+key=key.to_i
+
+symm_key2 = dh2.compute_key(key.to_bn)
+
+d = OpenSSL::Cipher.new(mode)
+d.decrypt
+
+case mode.strip 
+when "des-ecb"
+    d.key = symm_key2[0..7]
+when "des-ede3-cbc"
+    d.key = symm_key2[0..23]
+    d.iv = iv = client.gets.gsub(/\n$/, '')
+when "aes-cbc-192"
+    d.key = symm_key2[0..23]
+    d.iv = iv = client.gets.gsub(/\n$/, '')
+when 'idea-ofb'
+    d.iv = iv = client.gets.gsub(/\n$/, '')
+end
+
+message = client.gets
+message.delete!("\n")
+decrypted=d.update(message)+d.final
+
+puts
+puts "-----------------------------------"
+puts
+puts "Mode: " + mode
+puts "Public: " + (dh2.pub_key).to_s
+puts "Priv: " + (key).to_s
+puts "Sym Key 2: " + symm_key2
+puts "IV: " + iv.inspect
+puts "Decypted: " + decrypted
+puts
+puts "-----------------------------------"
+
+
+client.close
